c2638047705bd9b9417a16494bbcc960ca75584f
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-05-08 21:20:49 +0200

    Add radial animation

---
+++ Assets/Scripts/Animations/RadialAnimation.cs
@@ -0,0 +1,25 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform radialRt;
+    [SerializeField]
+    private RectTransform line;
+    [SerializeField]
+    private TweenData tweenData;
+
+    [SerializeField]
+    private float radialTime;
+    [SerializeField]
+    private float lineTime;
+
+    private void Update()
+    {
+        radialRt.transform.rotation = Quaternion.Euler(0, Time.time * radialTime * 360, 0);
+        line.transform.localRotation = Quaternion.Euler(0, 0, Time.time * lineTime * 360);;
+    }
+}
